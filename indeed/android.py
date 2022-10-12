a9139b88a9737ed7b4d2e671da25640e31517645
sarveshdubey <sarvesh91199@gmail.com>
2022-10-12 14:25:43 +0530

    headless

--- indeed/android.py
+++ indeed/android.py
@@ -15,10 +15,12 @@ import datetime
 from packaging.requirements import URL
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_argument('--headless')
 
 # profile = webdriver.FirefoxProfile("/Users/apple/Library/Application Support/Firefox/Profiles/0ur7nimh.default-release-11")
 # gecko_path="/Users/apple/doc/Git/geckodriver/geckodriver"
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
 
